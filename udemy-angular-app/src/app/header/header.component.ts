bb886aef57258d17c637aa56e4b09a535a74092b
Kamil <kamil.michalik@stanusch.com>
2019-10-08 18:58:48 +0200

    Next commit Section 7

--- udemy-angular-app/src/app/header/header.component.ts
+++ udemy-angular-app/src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+  feature: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(feature: string) {
+    this.feature = feature;
+    this.featureSelected.emit(this.feature);
+  }
 }
